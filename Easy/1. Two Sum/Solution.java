e3e800a3279ac9739e085af901ef2da6a734e91a
GPravin19 <pravin95petit@gmail.com>
2025-09-06 23:23:07 +0100

    two sum problem in java

---
+++ Easy/1. Two Sum/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+    
+        for(int i=1; i< nums.length; i++){
+            for(int j =i; j<nums.length; j++){
+                if(nums[j-i] + nums[j] == target){
+                    return new int[] {j, j-i};
+                }
+            }  
+        }
+        
+        return new int[2];
+    }
+}
